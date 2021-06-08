f02a030c60c796d3c381843960dbfa60ab1cbb33
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-06-08 23:18:01 +0300

    добавляет закрытие модалки

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,7 +1,10 @@
 import { refs } from '../refs.js'
 
 refs.filmListGallery.addEventListener('click', openModalWindow);
-
+refs.modalCloseBtn.addEventListener('click', onModalWindowCloseBtn);
+refs.lightbox.addEventListener('click', onOverlayClick);
+window.addEventListener('keydown', onEscPress);
+    
 function openModalWindow(e) {
     e.preventDefault();
     if (e.target.nodeName !== 'LI') return;
@@ -10,3 +13,23 @@ function openModalWindow(e) {
         //if()
         //currentInd = index;)
 }
+
+function onModalWindowCloseBtn() {
+    refs.lightbox.classList.remove('is-open');
+    refs.lightbox.removeEventListener('click', onOverlayClick);
+    window.removeEventListener('keydown', onEscPress);
+}
+
+function onOverlayClick(e) {
+    if (e.target === e.currentTarget) {
+        onModalWindowCloseBtn();
+    }
+}
+
+function onEscPress(e) {
+    if(e.code==='Escape'){
+        onModalWindowCloseBtn();
+    }
+}
+
+refs.modalCloseBtn.removeEventListener('click', onModalWindowCloseBtn);
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,6 +2,7 @@ const refs = {
   filmListGallery: document.querySelector('.film-list_gallery'),
   // filmCard: document.guerySelector('.js-film-card'),
   lightbox: document.querySelector('.js-lightbox'),
+  modalCloseBtn: document.querySelector('.modal-close-button'),
   //filmListItem: document.querySelector('.film-list_item'),
   // addWatchedBtn: document.guerySelector('.add-watched_button'),
   // addQueueBtn: document.guerySelector('.add-queue_button'),
