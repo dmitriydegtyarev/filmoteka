a81835c0ef4717a4f81caa255681e7717810c11c
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-06-09 21:52:28 +0300

    Photo packing was fixed

--- src/js/data/about-us.js
+++ src/js/data/about-us.js
@@ -2,7 +2,7 @@ export default [
   { name: 'Dmytro Dihtiarov', position: 'Team lead', photo: 'dmytro.jpg'},
   { name: 'Tatiana Taranushchenko', position: 'Scrum master', photo: '/images/about-us/tatiana.jpg'},
   { name: 'Yashan Vladyslav', position: 'Developer', photo: 'images/about-us/vladyslav.jpg' },
-  { name: 'Nataliia Chernieva', position: 'Developer', photo: 'images/about-us/nataliia.jpg' },
+  { name: 'Nataliia Chernieva', position: 'Developer', photo: 'images/about-us/natalia.jpg' },
   { name: 'Stas Kotelnykov', position: 'Developer', photo: 'images/about-us/stas.jpg' },
   { name: 'Olga Moiseyenko', position: 'Developer', photo: 'images/about-us/olga.jpg' },
 ];
