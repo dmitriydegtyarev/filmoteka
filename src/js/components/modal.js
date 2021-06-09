0e36ab32a2c9a6964bbad88be3e0f36ac05dc19f
Dmitriy Dihtiarov <dmitriydegtyarev1985@gmail.com>
2021-06-09 21:41:50 +0300

    update fix refs, modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,65 +1,63 @@
-// import { refs } from '../refs.js';
-// import api from '../api/axios';
-// import filmCardTmp from '../../templates/film-card.hbs';
+import { refs } from '../refs.js';
+import api from '../api/axios';
+import filmCardTmp from '../../templates/film-card.hbs';
 
-// // откритие/закрытие модалки
+// откритие/закрытие модалки
 // refs.filmListGallery.addEventListener('click', openModalWindow);
 // refs.modalCloseBtn.addEventListener('click', onModalWindowCloseBtn);
 // refs.lightbox.addEventListener('click', onOverlayClick);
 // window.addEventListener('keydown', onEscPress);
-// // добавить в просмотренные или в список просмотра
+// добавить в просмотренные или в список просмотра
 // refs.addWatchedBtn.addEventListener('click', onAddWatchedBtnClick);
 // refs.addQueueBtn.addEventListener('click', onAddQueueBtnClick);
 
-// function openModalWindow(e) {
-//     e.preventDefault();
-//     if (e.target.nodeName !== 'LI') return;
-//     refs.lightbox.classList.add('is-open');
-//     console.log('e.target.movieId :>> ', e.target.movieId);
-//     api.id = e.target.movieId;
+function openModalWindow(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'LI') return;
+  refs.lightbox.classList.add('is-open');
+  console.log('e.target.movieId :>> ', e.target.movieId);
+  api.id = e.target.movieId;
 
-//     fetchFilm();
-// }
+  fetchFilm();
+}
 
-// function fetchFilm(id){
-//    return api
-//        .getMovieById()
-//        .then(renderFilmMarkup);
-// }
+function fetchFilm(id) {
+  return api.getMovieById().then(renderFilmMarkup);
+}
 
-// function renderFilmMarkup(film){
-//    refs.filmCard.insertAdjancentHTML('beforeend', filmCardTmp(film));
-// }
+function renderFilmMarkup(film) {
+  refs.filmCard.insertAdjancentHTML('beforeend', filmCardTmp(film));
+}
 
-// function onAddWatchedBtnClick(id) {
-//     // дописать логику
+function onAddWatchedBtnClick(id) {
+  // дописать логику
 
-//     refs.addWatchedBtn.classList.add('press-btn');
-//     refs.addWatchedBtn.textContent = 'Added to Watched';
-// }
+  refs.addWatchedBtn.classList.add('press-btn');
+  refs.addWatchedBtn.textContent = 'Added to Watched';
+}
 
-// function onAddQueueBtnClick(id) {
-//     // дописать логику
-//     refs.addQueueBtn.classList.add('press-btn');
-//     refs.addQueueBtn.textContent = 'Added to Queue';
-// }
+function onAddQueueBtnClick(id) {
+  // дописать логику
+  refs.addQueueBtn.classList.add('press-btn');
+  refs.addQueueBtn.textContent = 'Added to Queue';
+}
 
-// function onModalWindowCloseBtn() {
-//     refs.lightbox.classList.remove('is-open');
-//     refs.lightbox.removeEventListener('click', onOverlayClick);
-//     window.removeEventListener('keydown', onEscPress);
-// }
+function onModalWindowCloseBtn() {
+  refs.lightbox.classList.remove('is-open');
+  refs.lightbox.removeEventListener('click', onOverlayClick);
+  window.removeEventListener('keydown', onEscPress);
+}
 
-// function onOverlayClick(e) {
-//     if (e.target === e.currentTarget) {
-//         onModalWindowCloseBtn();
-//     }
-// }
+function onOverlayClick(e) {
+  if (e.target === e.currentTarget) {
+    onModalWindowCloseBtn();
+  }
+}
 
-// function onEscPress(e) {
-//     if(e.code==='Escape'){
-//         onModalWindowCloseBtn();
-//     }
-// }
+function onEscPress(e) {
+  if (e.code === 'Escape') {
+    onModalWindowCloseBtn();
+  }
+}
 
-// refs.modalCloseBtn.removeEventListener('click', onModalWindowCloseBtn);
+refs.modalCloseBtn.removeEventListener('click', onModalWindowCloseBtn);
