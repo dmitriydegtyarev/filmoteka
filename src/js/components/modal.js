56ad3ea9f74a16ba21648ec2a094808b9368c2ec
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-06-10 00:57:04 +0300

    редактирует стили,шаблон и рефс

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -2,28 +2,30 @@ import { refs } from '../refs.js';
 import api from '../api/axios';
 import filmCardTmp from '../../templates/film-card.hbs';
 
-// откритие/закрытие модалки
-// refs.filmListGallery.addEventListener('click', openModalWindow);
-// refs.modalCloseBtn.addEventListener('click', onModalWindowCloseBtn);
-// refs.lightbox.addEventListener('click', onOverlayClick);
-// window.addEventListener('keydown', onEscPress);
-// добавить в просмотренные или в список просмотра
-// refs.addWatchedBtn.addEventListener('click', onAddWatchedBtnClick);
-// refs.addQueueBtn.addEventListener('click', onAddQueueBtnClick);
+//откритие/закрытие модалки
+refs.filmListGallery.addEventListener('click', openModalWindow);
+refs.modalCloseBtn.addEventListener('click', onModalWindowCloseBtn);
+refs.lightbox.addEventListener('click', onOverlayClick);
+window.addEventListener('keydown', onEscPress);
+//добавить в просмотренные или в список просмотра
+refs.addWatchedBtn.addEventListener('click', onAddWatchedBtnClick);
+refs.addQueueBtn.addEventListener('click', onAddQueueBtnClick);
 
 function openModalWindow(e) {
-  e.preventDefault();
-  if (e.target.nodeName !== 'LI') return;
+  // e.preventDefault();
+  if (e.target.nodeName !== 'IMG') return;
   refs.lightbox.classList.add('is-open');
-  console.log('e.target.movieId :>> ', e.target.movieId);
-  api.id = e.target.movieId;
-
-  fetchFilm();
+  const imgId = e.target.id;
+  console.log('e.target.Id :>> ', imgId);
+  //api.id = e.target.movieId;
+  
+  //return api.getMovieById(imgId);
+  //fetchFilm();
 }
 
-function fetchFilm(id) {
-  return api.getMovieById().then(renderFilmMarkup);
-}
+// function fetchFilm(id) {
+//   return api.getMovieById().then(renderFilmMarkup);
+// }
 
 function renderFilmMarkup(film) {
   refs.filmCard.insertAdjancentHTML('beforeend', filmCardTmp(film));
@@ -34,12 +36,14 @@ function onAddWatchedBtnClick(id) {
 
   refs.addWatchedBtn.classList.add('press-btn');
   refs.addWatchedBtn.textContent = 'Added to Watched';
+  refs.addWatchedBtn.disabled = true;
 }
 
 function onAddQueueBtnClick(id) {
   // дописать логику
   refs.addQueueBtn.classList.add('press-btn');
   refs.addQueueBtn.textContent = 'Added to Queue';
+  refs.addQueueBtn.disabled = true;
 }
 
 function onModalWindowCloseBtn() {
@@ -49,6 +53,8 @@ function onModalWindowCloseBtn() {
 }
 
 function onOverlayClick(e) {
+  console.log('e.target :>> ', e.target);
+  console.log('e.currentTarget :>> ', e.currentTarget);
   if (e.target === e.currentTarget) {
     onModalWindowCloseBtn();
   }
