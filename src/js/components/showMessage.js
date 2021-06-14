a8f050b13bac1d0bdc74653d14b05a215a0b5842
Vladyslav84 <fakingbrutal@gmail.com>
2021-06-14 15:47:21 +0300

    Зробив маленьки поправки: хедера, кнопок.

--- src/js/components/showMessage.js
+++ src/js/components/showMessage.js
@@ -6,14 +6,14 @@ export default function showMessage(response) {
     if (response.data.results.length === 0)
     {
         toastify({
-            text: 'Search result not successful. Enter the correct movie name and try again...',
+            text: 'Enter the correct movie name and try again...',
             duration: 2000,
             className: 'toastify-center_error',
         }).showToast();
     } else
     {
         toastify({
-            text: `search successful: ${ response.data.total_results } founded`,
+            text: `Search successful: ${ response.data.total_results } founded`,
             duration: 2000,
             className: 'toastify-center_successful',
         }).showToast();
