3fbe6f1b8022f49eb1437a172c65903316eaf421
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-06-16 19:01:29 +0300

    Add refs and function onLinkClick

--- src/js/refs.js
+++ src/js/refs.js
@@ -8,6 +8,7 @@ const refs = {
   aboutUs: document.querySelector('#about-us'),
   btnUp: document.querySelector('#btn-up'),
   filmListItem: document.querySelector('.film-list_item'),
+  linkTrailer: document.querySelector('.link film-trailer'),
   mainSection: document.querySelector('.section-main'),
 
   modal: document.querySelector('.modal'),
