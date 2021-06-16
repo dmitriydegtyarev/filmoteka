22448fb5d48ea433d07400f2f563471ad1660f64
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-06-16 23:15:34 +0300

    htafrnjhbyu

--- src/js/components/trailer.js
+++ src/js/components/trailer.js
@@ -1,16 +1,12 @@
 export default async function changeHomePage(result) {
   const baseYourtubeLinks = 'https://www.youtube.com/embed/';
 
-  let homepage = null;
-
   if (result[0] === undefined) {
     console.log('Нет трейлера');
-    return homepage;
-  } else {
-    console.log('Есть ключ для трейлера');
-    const yourtubeId = result[0].key;
-    console.log(yourtubeId);
-    homepage = `${baseYourtubeLinks}${yourtubeId}`;
+    return null;
   }
-  return homepage;
+  console.log('Есть ключ для трейлера');
+  const yourtubeId = result[0].key;
+  console.log(yourtubeId);
+  return `${baseYourtubeLinks}${yourtubeId}`;
 }
